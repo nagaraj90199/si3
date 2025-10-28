32a97a993291ce85f483492c521d7b08acf5c86b
nagaraj90199 <kn733478@gmail.com>
2025-10-28 11:31:38 +0530

    save

---
+++ studentdetails.py
@@ -0,0 +1,15 @@
+name="Nagaraj kuntoji"
+age=20
+email="01fe24bca049@kletech.ac.in"
+usn="01fe24bca049"
+branch="BCA"
+college="KLE Technological University"
+phone=9019955699
+print("Student Details:")
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
+print("USN:", usn)
+print("Branch:", branch)
+print("College:", college)
+print("Phone:", phone)
